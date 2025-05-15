c6fe71ba8a5838064addd244ce6d387bb06d249c
MisaelGG11 <magomez@innovacion.gob.sv>
2025-05-15 15:37:20 -0600

    first commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
+
+<script setup lang="ts">
+import { useHead } from '@vueuse/head';
+
+useHead({
+  title: 'Acerca de - Vue Meta Demo',
+  meta: [
+    { name: 'description', content: 'Página acerca de con metadatos SEO' }
+  ]
+});
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import TheWelcome from '../components/TheWelcome.vue'
+
+import { useHead } from '@vueuse/head';
+
+useHead({
+  title: 'Inicio - Vue Meta Demo',
+  meta: [
+    { name: 'description', content: 'Página de inicio con metadatos SEO en Vue 3 y prueba para minecraft' },
+    { name: 'keywords', content: 'vue, vue3, vue-router, vue-meta, minecraft' },
+    { name: 'author', content: 'Misael Gómez' },
+    { property: 'og:title', content: 'Inicio - Vue Meta Demo' },
+    { property: 'og:description', content: 'Página de inicio con metadatos SEO en Vue 3 y prueba para minecraft' },
+    { property: 'og:image', content: '/path/to/image.jpg' },
+    { property: 'og:url', content: window.location.href }
+  ]
+});
+</script>
+
+<template>
+  <main>
+    <TheWelcome />
+  </main>
+</template>
